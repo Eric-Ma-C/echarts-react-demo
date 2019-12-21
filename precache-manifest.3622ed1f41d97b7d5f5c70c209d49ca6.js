45fb62d392522d9fb5eaa7b7b98aec4f8f3e8e5d
EricMa <eric315@qq.com>
2019-12-21 22:15:46 +0800

    Updates

---
+++ precache-manifest.3622ed1f41d97b7d5f5c70c209d49ca6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "926036ec8ce0f96d3ba20c5d3ba1173d",
+    "url": "/echarts-react-demo/index.html"
+  },
+  {
+    "revision": "3fb7b7bd986fb72a6c1a",
+    "url": "/echarts-react-demo/static/js/2.116f0dae.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/echarts-react-demo/static/js/2.116f0dae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36052d4897acea6fffd9",
+    "url": "/echarts-react-demo/static/js/main.c6bea5f4.chunk.js"
+  },
+  {
+    "revision": "5cfa9a3ccd44ed933bdf",
+    "url": "/echarts-react-demo/static/js/runtime-main.70db4828.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/echarts-react-demo/precache-manifest.3622ed1f41d97b7d5f5c70c209d49ca6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/echarts-react-demo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
